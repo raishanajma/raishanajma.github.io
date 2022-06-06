c24a56a1fe4544a2658895b21ea98964cb87128c
Raisha Najma <80536213+raishanajma@users.noreply.github.com>
2022-06-07 03:01:42 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-class App extends React.Component {
+class App extends React.Component{
  render() {
      return (
          <div>
             <h1> Hello World </h1>
-            <p> Let's study React together! </p>
+            <p> Let's study together! </p>
          </div>
      );
  }
